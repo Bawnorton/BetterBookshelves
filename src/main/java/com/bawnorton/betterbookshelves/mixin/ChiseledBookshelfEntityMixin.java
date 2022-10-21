718f5d2247600b80f89e004693c3f149c2a54ccb
Benjamin Norton <benjamin@Benjamins-MacBook-Pro.local>
2022-10-21 15:01:31 -0700

    Get/Set bookshelf at index

--- src/main/java/com/bawnorton/betterbookshelves/mixin/ChiseledBookshelfEntityMixin.java
+++ src/main/java/com/bawnorton/betterbookshelves/mixin/ChiseledBookshelfEntityMixin.java
@@ -1,34 +1,103 @@
 package com.bawnorton.betterbookshelves.mixin;
 
 import com.bawnorton.betterbookshelves.BetterBookshelves;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.ChiseledBookshelfBlockEntity;
+import net.minecraft.inventory.Inventories;
 import net.minecraft.item.ItemStack;
-import org.spongepowered.asm.mixin.Final;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.tag.ItemTags;
+import net.minecraft.util.collection.DefaultedList;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.Collection;
-import java.util.Deque;
-import java.util.List;
+import java.util.Hashtable;
+
+import static com.bawnorton.betterbookshelves.BetterBookshelves.LOGGER;
 
 @Mixin(ChiseledBookshelfBlockEntity.class)
 public class ChiseledBookshelfEntityMixin {
-    @Shadow @Final private Deque<ItemStack> books;
+    private static final Hashtable<ChiseledBookshelfBlockEntity, DefaultedList<ItemStack>> bookWrapper = new Hashtable<>();
+
+    private DefaultedList<ItemStack> getBookList() {
+        DefaultedList<ItemStack> defaultedList = bookWrapper.get(((ChiseledBookshelfBlockEntity)(Object)this));
+        if (defaultedList == null) {
+            defaultedList = DefaultedList.ofSize(6, ItemStack.EMPTY);
+            bookWrapper.put(((ChiseledBookshelfBlockEntity)(Object)this), defaultedList);
+        }
+        return defaultedList;
+    }
 
     @Inject(method = "getLastBook", at=@At("HEAD"), cancellable = true)
     private void getLastBook(CallbackInfoReturnable<ItemStack> cir) {
         if(BetterBookshelves.requestIndex != -1) {
-            ItemStack[] items = books.toArray(ItemStack[]::new);
             try {
-                ItemStack book = items[BetterBookshelves.requestIndex];
+                ItemStack book = getBookList().set(BetterBookshelves.requestIndex, ItemStack.EMPTY);
                 cir.setReturnValue(book);
             } catch (ArrayIndexOutOfBoundsException e) {
                 cir.setReturnValue(ItemStack.EMPTY);
             }
             BetterBookshelves.requestIndex = -1;
         }
+    }
+
+    @Inject(method = "addBook", at=@At("HEAD"), cancellable = true)
+    private void addBook(ItemStack stack, CallbackInfo ci) {
+        if(stack.isIn(ItemTags.BOOKSHELF_BOOKS)) {
+            if(getBookList().get(BetterBookshelves.requestIndex) == ItemStack.EMPTY) {
+                getBookList().set(BetterBookshelves.requestIndex, stack);
+            }
+        }
+        ci.cancel();
+    }
+
+    @Inject(method = "readNbt", at=@At("HEAD"), cancellable = true)
+    private void readNbt(NbtCompound nbt, CallbackInfo ci) {
+        Inventories.readNbt(nbt, getBookList());
+        ci.cancel();
+    }
+
+    @Inject(method = "writeNbt", at=@At("HEAD"), cancellable = true)
+    private void writeNbt(NbtCompound nbt, CallbackInfo ci) {
+        Inventories.writeNbt(nbt, getBookList(), true);
+        ci.cancel();
+    }
+
+    @Inject(method ="toInitialChunkDataNbt", at=@At("HEAD"), cancellable = true)
+    private void toInitialChunkDataNbt(CallbackInfoReturnable<NbtCompound> cir) {
+        NbtCompound nbt = new NbtCompound();
+        Inventories.writeNbt(nbt, getBookList(), true);
+        cir.setReturnValue(nbt);
+    }
+
+    @Inject(method = "clear", at=@At("HEAD"), cancellable = true)
+    public void clear(CallbackInfo ci) {
+        getBookList().clear();
+        ci.cancel();
+    }
+
+    @Inject(method = "getBookCount", at=@At("HEAD"), cancellable = true)
+    public void getBookCount(CallbackInfoReturnable<Integer> cir) {
+        int count = 0;
+        for(ItemStack stack : getBookList()) {
+            if(stack != ItemStack.EMPTY) {
+                count++;
+            }
+        }
+        cir.setReturnValue(count);
+    }
+
+    @Inject(method = "isFull", at=@At("HEAD"), cancellable = true)
+    public void isFull(CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(((ChiseledBookshelfBlockEntity)(Object)(this)).getBookCount() == 6);
+    }
+
+    @Inject(method = "isEmpty", at=@At("HEAD"), cancellable = true)
+    public void isEmpty(CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(((ChiseledBookshelfBlockEntity)(Object)(this)).getBookCount() == 0);
     }
 }
--- src/main/java/com/bawnorton/betterbookshelves/mixin/ChiseledBookshelfMixin.java
+++ src/main/java/com/bawnorton/betterbookshelves/mixin/ChiseledBookshelfMixin.java
@@ -2,16 +2,17 @@ package com.bawnorton.betterbookshelves.mixin;
 
 import com.bawnorton.betterbookshelves.BetterBookshelves;
 import com.bawnorton.betterbookshelves.Book;
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.ChiseledBookshelfBlock;
 import net.minecraft.block.entity.ChiseledBookshelfBlockEntity;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.state.property.IntProperty;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
@@ -20,27 +21,61 @@ import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.RaycastContext;
 import net.minecraft.world.World;
 import net.minecraft.world.event.GameEvent;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 import static com.bawnorton.betterbookshelves.BetterBookshelves.LOGGER;
-import static net.minecraft.block.ChiseledBookshelfBlock.BOOKS_STORED;
-import static net.minecraft.block.ChiseledBookshelfBlock.LAST_INTERACTION_BOOK_SLOT;
 
 @Mixin(ChiseledBookshelfBlock.class)
 public class ChiseledBookshelfMixin {
+
+    @Shadow @Final public static IntProperty BOOKS_STORED;
+
+    @Shadow @Final public static IntProperty LAST_INTERACTION_BOOK_SLOT;
+
+    @ModifyArg(method = "<init>", at=@At(value = "INVOKE", target = "Lnet/minecraft/block/BlockWithEntity;<init>(Lnet/minecraft/block/AbstractBlock$Settings;)V"))
+    private static AbstractBlock.Settings modifySettings(AbstractBlock.Settings settings) {
+        return settings.nonOpaque();
+    }
+
     @Redirect(method = "onUse", at=@At(value = "INVOKE", target = "Lnet/minecraft/block/ChiseledBookshelfBlock;tryRemoveBook(Lnet/minecraft/block/BlockState;Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/block/entity/ChiseledBookshelfBlockEntity;)Lnet/minecraft/util/ActionResult;"))
     private ActionResult tryRemoveLookingAtBook(BlockState state, World world, BlockPos pos, PlayerEntity player, ChiseledBookshelfBlockEntity blockEntity) {
         if (!blockEntity.isEmpty()) {
-            ItemStack itemStack = getLookingAtBook((ServerPlayerEntity) player, blockEntity);
+            updateLookingAtIndex((ServerPlayerEntity) player, blockEntity);
+            ItemStack itemStack = blockEntity.getLastBook();
             LOGGER.info("Looking at book: " + itemStack.getNbt());
-
+            world.playSound(null, pos, SoundEvents.BLOCK_WOOD_PLACE, SoundCategory.BLOCKS, 1.0F, 1.0F);
+            int i = blockEntity.getBookCount();
+            world.setBlockState(pos, state.with(BOOKS_STORED, i).with(LAST_INTERACTION_BOOK_SLOT, i + 1), 3);
+            world.emitGameEvent(player, GameEvent.BLOCK_CHANGE, pos);
+            if(!player.getInventory().insertStack(itemStack)) {
+                player.dropItem(itemStack, false);
+            }
         }
         return ActionResult.CONSUME;
     }
 
-    private ItemStack getLookingAtBook(ServerPlayerEntity player, ChiseledBookshelfBlockEntity blockEntity) {
+    @Redirect(method = "onUse", at=@At(value = "INVOKE", target = "Lnet/minecraft/block/ChiseledBookshelfBlock;tryAddBook(Lnet/minecraft/block/BlockState;Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/block/entity/ChiseledBookshelfBlockEntity;Lnet/minecraft/item/ItemStack;)Lnet/minecraft/util/ActionResult;"))
+    private ActionResult tryAddLookingAtBook(BlockState state, World world, BlockPos pos, PlayerEntity player, ChiseledBookshelfBlockEntity blockEntity, ItemStack stack) {
+        if (!blockEntity.isFull()) {
+            updateLookingAtIndex((ServerPlayerEntity) player, blockEntity);
+            blockEntity.addBook(stack.split(1));
+            world.playSound(null, pos, SoundEvents.BLOCK_WOOD_PLACE, SoundCategory.BLOCKS, 1.0F, 1.0F);
+            if(player.isCreative()) {
+                stack.increment(1);
+            }
+            int i = blockEntity.getBookCount();
+            world.setBlockState(pos, state.with(BOOKS_STORED, i).with(LAST_INTERACTION_BOOK_SLOT, i), 3);
+            world.emitGameEvent(player, GameEvent.BLOCK_CHANGE, pos);
+        }
+        return ActionResult.CONSUME;
+    }
+
+    private void updateLookingAtIndex(ServerPlayerEntity player, ChiseledBookshelfBlockEntity blockEntity) {
         MinecraftClient client = MinecraftClient.getInstance();
         Vec3d cameraDir = client.getCameraEntity().getRotationVec(1.0F);
         Vec3d cameraPos = client.getCameraEntity().getCameraPosVec(1.0F);
@@ -67,11 +102,9 @@ public class ChiseledBookshelfMixin {
             isFrontFace = true;
             x = 16 - (int) (hitPosRel.z * 16);
         }
-        if (!isFrontFace) {
-            return ItemStack.EMPTY;
+        if (isFrontFace) {
+            Book book = Book.getBook(x, y);
+            BetterBookshelves.requestIndex = book.index();
         }
-        Book book = Book.getBook(x, y);
-        BetterBookshelves.requestIndex = book.index();
-        return blockEntity.getLastBook();
     }
 }
