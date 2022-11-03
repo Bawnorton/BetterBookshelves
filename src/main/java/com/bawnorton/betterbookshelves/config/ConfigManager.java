925773bf524e5b72dd3deae45674f63f9830caa1
Benjamin Norton <bawnorton@me.com>
2022-11-03 15:02:51 -0700

    Remove in-game config until yacl ported

--- src/main/java/com/bawnorton/betterbookshelves/config/ConfigManager.java
+++ src/main/java/com/bawnorton/betterbookshelves/config/ConfigManager.java
@@ -4,14 +4,8 @@ import com.bawnorton.betterbookshelves.BetterBookshelves;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonSyntaxException;
-import dev.isxander.yacl.api.ConfigCategory;
-import dev.isxander.yacl.api.Option;
-import dev.isxander.yacl.api.YetAnotherConfigLib;
-import dev.isxander.yacl.gui.controllers.cycling.EnumController;
-import dev.isxander.yacl.gui.controllers.slider.IntegerSliderController;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.text.Text;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -24,39 +18,7 @@ public class ConfigManager {
     private static final Path configPath = FabricLoader.getInstance().getConfigDir().resolve("betterbookshelves.json");
 
     public static Screen createGui(Screen parent) {
-        return YetAnotherConfigLib.createBuilder()
-                .title(Text.of("BetterBookshelves Config"))
-                .category(ConfigCategory.createBuilder()
-                        .name(Text.of("General"))
-                        .tooltip(Text.of("General settings"))
-                        .option(Option.createBuilder(Config.TextPreview.class)
-                                .name(Text.of("Text Preview"))
-                                .tooltip(Text.of("How to display the text preview"))
-                                .binding(BetterBookshelves.CONFIG.textPreview,
-                                        () -> BetterBookshelves.CONFIG.textPreview,
-                                        value -> BetterBookshelves.CONFIG.textPreview = value)
-                                .controller(EnumController::new)
-                                .build())
-                        .option(Option.createBuilder(Config.BookTexture.class)
-                                .name(Text.of("Book Texture"))
-                                .tooltip(Text.of("How to display the book texture"))
-                                .binding(BetterBookshelves.CONFIG.bookTexture,
-                                        () -> BetterBookshelves.CONFIG.bookTexture,
-                                        value -> BetterBookshelves.CONFIG.bookTexture = value)
-                                .controller(EnumController::new)
-                                .build())
-                        .option(Option.createBuilder(Integer.class)
-                                .name(Text.of("Text Size"))
-                                .tooltip(Text.of("The size of the text preview"))
-                                .binding(BetterBookshelves.CONFIG.textSize,
-                                        () -> BetterBookshelves.CONFIG.textSize,
-                                        value -> BetterBookshelves.CONFIG.textSize = value)
-                                .controller(integerOption -> new IntegerSliderController(integerOption, 5, 20, 1))
-                                .build())
-                        .build())
-                .save(ConfigManager::save)
-                .build()
-                .generateScreen(parent);
+        return parent;
     }
 
     public static void loadConfig() {
